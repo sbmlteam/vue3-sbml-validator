4bcda65b64a811b1c8a77f9957c22826f0812c33
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2026-02-10 15:39:13 +0100

    - set base dir

--- src/lib/validatorAdapter.js
+++ src/lib/validatorAdapter.js
@@ -1,4 +1,7 @@
-const LIB_BASE = '/lib/'
+const BASE = typeof import.meta !== 'undefined' && import.meta.env && import.meta.env.BASE_URL
+  ? import.meta.env.BASE_URL
+  : '/'
+const LIB_BASE = BASE.replace(/\/?$/, '/') + 'lib/'
 
 let modulePromise = null
 
