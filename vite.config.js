e6c9916175fe6063fa0b4aec4919cf00103f6794
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2026-02-10 16:19:43 +0100

    - build for hugo optionally

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const buildForHugo = process.env.BUILD_FOR_HUGO === '1'
+
 export default defineConfig({
   base: '/vue3-sbml-validator/',
   plugins: [vue()],
+  build: buildForHugo
+    ? {
+        rollupOptions: {
+          output: {
+            entryFileNames: 'assets/[name].js',
+            chunkFileNames: 'assets/[name].js',
+            assetFileNames: 'assets/[name].[ext]',
+          },
+        },
+      }
+    : undefined,
 })
