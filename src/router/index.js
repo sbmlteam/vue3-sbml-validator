09aaf6861669420d61333d20bcb55d0a1b8e05ed
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2026-04-08 10:31:22 +0200

    try to get around hugo issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import ValidatorApp from '../components/SbmlValidator.vue'  
 import HelpPage from '../components/HelpPage.vue'
+
+const STANDALONE_BASE = '/vue3-sbml-validator/'
+
+function pickHistory() {
+  // When the built bundle is embedded into another site (e.g. Hugo),
+  // the hosting page path usually won't be under STANDALONE_BASE.
+  // In that case, history-mode routing won't match and the app renders blank.
+  // Hash history keeps routing self-contained and works from any host path.
+  if (typeof window !== 'undefined' && window.location.pathname.startsWith(STANDALONE_BASE)) {
+    return createWebHistory(STANDALONE_BASE)
+  }
+  return createWebHashHistory()
+}
 
 const routes = [
   { path: '/',     component: ValidatorApp },
@@ -8,6 +21,6 @@ const routes = [
 ]
 
 export default createRouter({
-  history: createWebHistory('/vue3-sbml-validator/'),
+  history: pickHistory(),
   routes,
 })
